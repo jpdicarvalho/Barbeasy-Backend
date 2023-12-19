4fa2a56b7ae3846d18c9c348eab77700bee61fd7
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2023-12-19 15:47:43 -0300

    the new api-barbeasy-client

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Rota principal que exibe "API running" na tela
+app.get('/', (req, res) => {
+  res.send('API running');
+});
+
+// Inicia o servidor na porta especificada
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
