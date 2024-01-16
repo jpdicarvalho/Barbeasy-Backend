b27796e815877d748c9a254c58b5d9944324e581
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-01-16 16:32:34 -0300

    install dependencies aws-s3

--- Controller.js
+++ Controller.js
@@ -1,13 +1,18 @@
 // index.js
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config'
 import bodyParser from "body-parser";
+
 import mysql from "mysql2";
-import multer from 'multer';
-import path from 'path';
+
 import jwt  from 'jsonwebtoken';
 import MercadoPago from "mercadopago";
+
+import multer from 'multer';
+import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+import 'dotenv/config'
 
 const app = express();
 const port = process.env.PORT || 3000;
