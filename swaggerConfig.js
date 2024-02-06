4efd19b4207c9f989f1d6ab0b53e9fadf224bb56
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-02-05 22:17:39 -0300

    update documentation

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -5,24 +5,6 @@ import './swaggerComments.js';
 
 // Defina as opções do Swagger
 const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      title: 'Barbeasy-API',
-      description: "### Barbeasy API é uma plataforma intuitiva para realizar agendamento e pagamentos de serviços de barbearias.",
-      version: '1.0.0',
-      contact: {
-        name: 'jp.dicarvalho',
-        email: 'joaopedro.ufopa@email.com',
-      },
-    },
-    servers: [
-      {
-        url: 'http://localhost:8000', // URL do servidor local
-        description: 'Servidor Local',
-      },
-      // Adicione mais objetos de servidor conforme necessário para ambientes de desenvolvimento, teste e produção
-    ],
-  },
   apis: ['./swaggerComments.js'], // Caminho para os arquivos de rotas da sua API
 };
 
