c2bf8980fc26cd6591caf783a0454e0a81fab077
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-04-19 21:50:23 -0300

    Update Controller.js

--- Controller.js
+++ Controller.js
@@ -1170,8 +1170,12 @@ app.get('/api/bookings/:barbeariaId/:professionalId/:selectedDate', (req, res) =
       return res.status(500).json({ Error: 'Internal Server Error.' }); 
     }
     if(result.length > 0){
-      return res.status(200).json({Success: "Success", allBookings: result})
+      res.status(200).json({Success: "Success", allBookings: result})
+    }else{
+      console.log('caiu aqui')
+      res.status(404).json({NotFound: "NotFound" });
     }
+    
   })
 });
 
