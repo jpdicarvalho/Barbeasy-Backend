2afc5257f682d2ee206a5a6c49a71c99e24e48bf
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-06-26 12:50:29 -0300

    Update UseBarbeariaAuthenticateJWT.js

--- UseBarbeariaAuthenticateJWT.js
+++ UseBarbeariaAuthenticateJWT.js
@@ -1,39 +1,40 @@
-import jwt  from 'jsonwebtoken';
-
-import 'dotenv/config'
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 const SECRET_TOKEN_BARBEARIA = process.env.TOKEN_SECRET_WORD_OF_USER_BARBEARIA;
 const SECRET_TOKEN_USER_CLIENT = process.env.TOKEN_SECRET_WORD_OF_USER_CLIENT;
 
-
 if (!SECRET_TOKEN_BARBEARIA) {
-    throw new Error('TOKEN_SECRET is not defined in environment variables.');
-  }
-  
-  const UseBarbeariaAuthenticateJWT = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-  
-    if (authHeader) {
-      const token = authHeader.split(' ')[1];
-  
-      jwt.verify(token, SECRET_TOKEN_BARBEARIA, (err, user) => {
-        if (err) {
-          jwt.verify(token, SECRET_TOKEN_USER_CLIENT, (erro, userClient) => {
-            if(erro){
-              return res.status(403).json({ message: 'Forbidden: Invalid token' });
-            }
+  throw new Error('TOKEN_SECRET_BARBEARIA is not defined in environment variables.');
+}
+
+if (!SECRET_TOKEN_USER_CLIENT) {
+  throw new Error('TOKEN_SECRET_USER_CLIENT is not defined in environment variables.');
+}
+
+const UseBarbeariaAuthenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, SECRET_TOKEN_BARBEARIA, (err, user) => {
+      if (err) {
+        jwt.verify(token, SECRET_TOKEN_USER_CLIENT, (erro, userClient) => {
+          if (erro) {
+            return res.status(403).json({ message: 'Forbidden: Invalid token' });
+          }
           req.userClient = userClient;
           next();
-          })
-          
-        }
-          
+        });
+      } else {
         req.user = user;
         next();
-      });
-    } else {
-      res.status(401).json({ message: 'Unauthorized: No token provided' });
-    }
-  };
-  
-  export default UseBarbeariaAuthenticateJWT;
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'Unauthorized: No token provided' });
+  }
+};
+
+export default UseBarbeariaAuthenticateJWT;
