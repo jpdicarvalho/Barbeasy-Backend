cdeaaeff3168d87459ad4a77a59b3c91c7d74312
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-07-23 19:22:03 -0300

    Update Controller.js

--- Controller.js
+++ Controller.js
@@ -71,7 +71,7 @@ const logger = winston.createLogger({// Configuração do Winston para registrar
 //===================== MIDDLEWARE TO RATE LIMIT =====================
 const limiter = rateLimit({// Configurar limitação de taxa
   windowMs: 3 * 60 * 1000, // 15 minutos
-  max: 10000, // Limite de 100 requisições por IP
+  max: 100000, // Limite de 100 requisições por IP
   message: 'Error in request'
 });
 
