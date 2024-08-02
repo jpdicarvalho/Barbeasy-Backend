d24320944a44ff19f05a3f8f75af7e0eea4d219e
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-08-02 15:10:32 -0300

    Update Controller.js

--- Controller.js
+++ Controller.js
@@ -779,7 +779,7 @@ const { userId, barbeariaId, professionalId, serviceId } = req.body;//To save pa
 
   // Definindo a data de expiração para 50 segundos a partir da data atual
   const expirationDate = new Date();
-  expirationDate.setSeconds(expirationDate.getSeconds() + 150);
+  expirationDate.setSeconds(expirationDate.getSeconds() + 60);
   const dateOfExpiration = expirationDate.toISOString(); // Formato ISO 8601
 
   const body = { 
